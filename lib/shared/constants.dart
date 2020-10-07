ebc77ef2c47f3ea3c7c8c2f30f816d8b916e9f27
Mai Duy Dũng <maiduydung@yahoo.com>
2020-10-07 16:09:12 +0900

    text decoration constant

---
+++ lib/shared/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const textInputDecoration = InputDecoration(
+  fillColor: Colors.cyanAccent,
+  filled: true,
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.white),
+  ),
+  focusedBorder:
+      OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+);
